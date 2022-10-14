e4d4a7b658156501c3cd0400e9daa04097b12fc0
Joshua wambua <jmutua.w@gmail.com>
2022-10-14 14:52:12 -0700

    print all except q,e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all except q and e
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	for (int n = 'a'; n <= 'z'; n++)
+	{
+		if (n != 'e' && n != 'q')
+		{
+			putchar(n);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
