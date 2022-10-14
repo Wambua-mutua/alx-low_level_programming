435c725bd14a2f69adfb00b4f78f851ffcc6924a
Joshua wambua <jmutua.w@gmail.com>
2022-10-14 13:59:24 -0700

    get alphabet in lower

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<stdlib.h>
+/**
+ * main- type the alphabets in lower case
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	for (int x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
