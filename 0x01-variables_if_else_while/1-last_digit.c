db1e981f0ce8f9c8e469e5d3b8d374d75bd7a132
Joshua wambua <jmutua.w@gmail.com>
2022-10-14 13:26:16 -0700

    check last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdioh.h>
+#include <time.h>
+
+/**
+ *main- dtermine if the last digit of a num <5>
+ *
+ *Return: Always 0(success)
+ */
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/*divide n by 10 and get modulous i.e last digit*/
+	int m = n % 10;
+
+	if (m > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+	}
+
+	else if (m == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, m);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+	}
+	return (0);
+}
