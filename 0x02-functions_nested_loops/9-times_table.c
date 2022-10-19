c4b11e5d6a8e032cd4e2ee7c2e646e7506794f78
Joshua wambua <jmutua.w@gmail.com>
2022-10-19 11:49:52 -0700

    multiple table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,12 @@ void times_table(void)
 {
 	int  i, j, k;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 48; j <= 57; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
-			_putchar(k);
+			_putchar(k + '0');
 		}
 	_putchar('\n');
 	}
