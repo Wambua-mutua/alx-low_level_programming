f2d4a69836c4fcede334cab9edb71fdf23201529
Joshua wambua <jmutua.w@gmail.com>
2022-10-19 13:00:24 -0700

    products table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,14 +10,24 @@ void times_table(void)
 
 	for (i = 1 ; i <= 9; i++)
 	{
+		_putchar('0');
 		for (j = 1; j <= 9; j++)
 		{
 			k = i * j;
 
-			_putchar(k);
 			_putchar(',');
 			_putchar(' ');
+			if (k <= 9)
+			{
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
-	_putchar('\n');
+	putchar('\n');
 	}
 }
