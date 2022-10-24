80d13e51064c0b20fae2dc37bb68e24b8bd728c8
Joshua wambua <jmutua.w@gmail.com>
2022-10-24 06:13:23 -0700

    print numbers betwee 0-9 except 2&4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_most_numbers - print int btwn 0-9 except 2&4
+*Return: no return.
+*/
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = '0'; x < '9'; x++)
+	{
+		if (x != '2' && x != '4')
+			_putchar(x);
+	}
+	_putchar('\n');
+}
