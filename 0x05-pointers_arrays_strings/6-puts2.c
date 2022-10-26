e5a116c906f174b4aa6f98e195df1ea7492bcbdc
Joshua wambua <jmutua.w@gmail.com>
2022-10-26 04:00:29 -0700

    prints ever other number

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*puts2 - prints one char out of 2 of a string.
+*@str: input string
+*Return: no return.
+*/
+void puts2(char *str)
+{
+	int num = 0;
+
+	while (num >= 0)
+	{
+		if (str[num] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (num % 2 == 0)
+			_putchar(str[num]);
+		num++;
+	}
+}
